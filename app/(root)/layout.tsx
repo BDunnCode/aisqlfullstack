d04b7b8f827920c86d0f14a9b977ee5d51e1c798
BDunnCode <BDunnCode@github.com>
2024-02-29 15:50:26 -0600

    add: sidebar && mobile-sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,5 @@
-import { Navbar } from "@/components/Navbar";
+import { Navbar } from "@/components/navbar";
+import { Sidebar } from "@/components/sidebar";
 
 
 
@@ -10,6 +11,9 @@ const RootLayout = ({
   return (
     <div className="h-full">
       <Navbar />
+      <div className="hidden md:flex mt-16 w-20 flex-col fixed inset-y-0">
+        <Sidebar />
+      </div>
       <main className="md:pl-20 pt-16 h-full">
         {children}
       </main>
