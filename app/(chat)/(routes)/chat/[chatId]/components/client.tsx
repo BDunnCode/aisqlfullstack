2b1519068e447213e1903f116b794852c0f69f88
BDunnCode <BDunnCode@github.com>
2024-03-10 13:28:55 -0500

    security fixes, chat client commit 1

--- app/(chat)/(routes)/chat/[chatId]/components/client.tsx
+++ app/(chat)/(routes)/chat/[chatId]/components/client.tsx
@@ -1,21 +1,41 @@
 "use client";
 
-import { ChatHeader } from "@/components/chat-header";
+import { useCompletion } from "ai/react";
+import { useState } from "react";
 import { Companion, Message } from "@prisma/client";
+import { useRouter } from "next/navigation";
+
+import { ChatHeader } from "@/components/chat-header";
+
 
 interface ChatClientProps {
   companion: Companion & {
       messages: Message[];
       _count: {
         messages: number;
-      }
-    }
-  }
-
+      };
+    };
+  };
 
 export const ChatClient = ({
   companion
 }: ChatClientProps) => {
+  const router = useRouter();
+  const [messages, setMessages] = useState<any[]>(companion.messages);
+
+  const {
+    input, 
+    isLoading,
+    handleInputChange,
+    handleSubmit,
+    setInput,
+  } = useCompletion({
+    api: `/api/chat/${companion.id}`,
+    onFinish(completion) {
+
+    },
+  });
+
   return (
     <div className="flex flex-col h-full p-4 space-y-2">
       <ChatHeader companion={companion} />
