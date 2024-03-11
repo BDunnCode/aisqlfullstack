6b9c878b89e170771380a9d7d95b30b22a8fd75e
BDunnCode <BDunnCode@github.com>
2024-03-11 13:30:00 -0500

    further develop chat-messages component, add chat-message component

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -2,16 +2,26 @@
 
 import { Companion } from "@prisma/client";
 
+import { ChatMessage } from "@/components/chat-message";
+
 interface ChatMessagesProps {
   messages: any[];
   isLoading: boolean;
   companion: Companion;
 };
 
-export const ChatMessages = () => {
+export const ChatMessages = ({
+  messages = [],
+  isLoading,
+  companion
+}: ChatMessagesProps) => {
   return (
-    <div>
-      ChatMessages
+    <div className="flex-1 overflow-y-auto pr-4">
+      <ChatMessage
+        src={companion.src}
+        role="system"
+        content={`Hello, I am ${companion.name}, ${companion.description}`} 
+      />
     </div>
   )
 }
